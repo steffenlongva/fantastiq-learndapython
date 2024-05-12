3b86cfca968064f8af6176fdd40815d379b92fd1
Steffen Andre Longva <steffen@longva.org>
2024-05-12 18:10:46 +0200

    removed a main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to the Band Name Generator 2000")
-city = input("What city did you grow up in? \n")
-pet = input("What is the name of a pet? \n")
-bandname = city + " " + pet
-print("Your band would be something like \n" + bandname)
